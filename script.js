dd34a39e6ecaf4f89d962bae696b7effd1aa8cfe
Your Name <you@example.com>
2021-01-15 14:25:05 +0600

    let capital

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let first = "Kutman"
+let first = "kutman"
 let second = first
 let third = second 
 let fouth = third 
 let fifth = fouth
+let capital = "London"
+let country = "GR"
+
+first = "shrek"
+
+console.log( capital + " is the capital of " + country)
 
