6bf837af0b29a7f62c5a791170031c61a9571107
Your Name <you@example.com>
2021-01-15 14:35:12 +0600

    let subject and unit

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ let second = first
 let third = second 
 let fouth = third 
 let fifth = fouth
-let capital = "London"
-let country = "GR"
+let capital = "Bishkek"
+let country = "Kuyrgyzstan"
+let subject = "Distance"
+let unit = "meters"
 
 first = "shrek"
 
 console.log( capital + " is the capital of " + country)
+console.log( subject + " is measured in " + unit)  
 
