cf9c99237d6ae633cf37cc8a13a8e537343547a2
Your Name <you@example.com>
2021-01-15 14:45:06 +0600

    let title and author

--- script.js
+++ script.js
@@ -8,8 +8,15 @@ let country = "Kuyrgyzstan"
 let subject = "Distance"
 let unit = "meters"
 
+let title = "Dark Town"
+let author = "Stiven King"
+
+subject = "jd'jdas"
+unit = "fa;fjdklsa"
+
 first = "shrek"
 
 console.log( capital + " is the capital of " + country)
 console.log( subject + " is measured in " + unit)  
+console.log("My favourite book is " + title + " by " + author)
 
